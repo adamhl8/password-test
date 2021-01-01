6234d5d0d09926ae6b9e1931ca44d68e42db8946
Adam Langbert <adamhl@pm.me>
2021-01-02 00:15:47 +0100

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,47 +2,49 @@ import bcrypt from "bcrypt"
 import prompts from "prompts"
 import fs, { promises as fsp } from "fs"
 
-const pwRegExp = /^(?=.*[a-zA-Z])(?=.*\s.*\s.*\s)(?=.*[4-9])(?!.*[$^&*()[\]]).{10,}$/gm
-const passwordExists = fs.existsSync("password")
-
-if (passwordExists) {
-  console.log("Password file found. Please enter the password.")
-} else console.log("No password file found. Please create a new password.")
-
-async function passwordPrompt() {
-  
-  if (!passwordExists) console.log(`
+const pwRequirements = `
 Password Requirements:
 
 At least 10 characters.
 At least 3 whitespace characters.
 At least one digit between 4 and 9.
 The following symbols are not allowed: $, ^, &, *, (, ), [, ]
-`)
+`
 
-  const response = await prompts({
-    type: "password",
-    name: "value",
-    message: "Enter password",
-  })
+const pwRegExp = /^(?=.*[a-zA-Z])(?=.*\s.*\s.*\s)(?=.*[4-9])(?!.*[$^&*()[\]]).{10,}$/gm
+const passwordExists = fs.existsSync("password")
 
+if (passwordExists) {
+  console.log("Password file found. Please enter the password.")
+} else { 
+  console.log("No password file found. Please create a new password.")
+}
+
+async function passwordPrompt() {
+  
   if (!passwordExists) {
 
-    if (response.value.match(pwRegExp)) {
-      console.log("Saving password...")
-      passwordSave(response.value)
-    } else {
-      console.log("== Password does not satisfy requirements. ==")
-      passwordPrompt()
-    }
+    console.log(pwRequirements)
+
+    const response = await prompts({
+      type: "password",
+      name: "value",
+      message: "Enter password",
+      validate: value => value.match(pwRegExp) ? true : "Password does not satisfy requirements."
+    })
+
+    console.log("Saving password...")
+    passwordSave(response.value)
   } else {
     
-    if (await passwordCheck(response.value)) {
-      console.log("Password matches!")
-    } else {
-      console.log("Incorrect password. Try again.")
-      passwordPrompt()
-    }
+    await prompts({
+      type: "password",
+      name: "value",
+      message: "Enter password",
+      validate: async value => await passwordCheck(value) ? true : "Incorrect password. Try again."
+    })
+
+    console.log("Password matches!")
   }
 }
 
